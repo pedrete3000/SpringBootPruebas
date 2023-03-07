a7645f786659e4b8d2fedd83edbccacb940aa686
pedrete3000 <pedrete3000@hotmail.com>
2023-03-07 05:53:44 +0100

    ejercicio 7 de Marzo

--- src/main/java/cat/itacademy/S04T01N01PedroTejero/contollers/HelloWorldController.java
+++ src/main/java/cat/itacademy/S04T01N01PedroTejero/contollers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package cat.itacademy.S04T01N01PedroTejero.contollers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,10 @@ public class HelloWorldController {
 	
 	 @GetMapping("/HelloWorld")
 	    public String saluda(@RequestParam(value = "name", defaultValue = "unknown") String name) {
+	      return String.format("Hello %s! estás ejecutando un proyecto Maven", name);
+	    }
+	 @GetMapping("/HelloWorld2/{name}")
+	    public String saluda2(@PathVariable (required=false) String name) {
 	      return String.format("Hello %s! estás ejecutando un proyecto Maven", name);
 	    }
 
